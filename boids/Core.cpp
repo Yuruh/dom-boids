7e74908d46e87a3a45f5ea9f4cfa26b43e9030af
Antoine Lempereur <antoine.lempereur@epitech.eu>
2020-07-13 11:36:01 +0200

    docker setup

--- boids/Core.cpp
+++ boids/Core.cpp
@@ -8,3 +8,7 @@
 void Core::displayNextMovements(int n, float refreshRateSec) {
     this->map.display();
 }
+
+//The main method should take as param the refresh rate, and the number of seconds to simulate.
+//It should return the map state for every possibility
+// E.G 60 FPS 2 seconds, 120 maps for every (1 / 60) sec
