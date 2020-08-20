243fca1cf54258ae36e669a7a3d3388032b38007
Antoine Lempereur <antoine.lempereur@epitech.eu>
2020-08-20 15:41:40 +0200

    draft of the page

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import {SliderSection} from "./SliderSection";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to rerezload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>DOM BOIDS</h1>
+      <h4>Teeming the web with life</h4>
+      <h6>By <a href={"https://github.com/yuruh"} target={"_blank"} rel="noopener noreferrer">Antoine Lempereur</a></h6>
+      <p>Based on Craig Reynolds BOIDS model, stuff that should be said so ppl understand what's going on</p>
+      <hr/>
+      <h2>Configuring the simulation</h2>
+      <SliderSection title={"Alignment"} explanation={"Boids should always steer towards the average heading of local flockmates"} value={1} onSliderValueChange={() => {}}/>
+      <div className={"align-right"}>
+        <SliderSection title={"Alignment"} explanation={"Boids should always steer towards the average heading of local flockmates"} value={1} onSliderValueChange={() => {}}/>
+      </div>
+      <SliderSection title={"Alignment"} explanation={"Boids should always steer towards the average heading of local flockmates"} value={1} onSliderValueChange={() => {}}/>
+      <div className={"align-right"}>
+        <SliderSection title={"Alignment"} explanation={"Boids should always steer towards the average heading of local flockmates"} value={1} onSliderValueChange={() => {}}/>
+      </div>
+      <hr/>
+      <h2>About the project</h2>
+      <div className={"section"}>About<p>Making a complex behaviour emerge from simple rules. Transforming a web page in the boids environment</p></div>
+      <div className={"section"}>Technical Specificities</div>
+      <div className={"section"}>Development</div>
     </div>
   );
 }
